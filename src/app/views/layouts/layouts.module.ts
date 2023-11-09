12837bf5acffdf46202eb5cba2aae6019cbe412e
RAJEEV KUAMR <96237505+Rajeevkumar69@users.noreply.github.com>
2023-11-09 19:43:12 +0530

    Virified listings

--- src/app/views/layouts/layouts.module.ts
+++ src/app/views/layouts/layouts.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from '../main/home/home.component';
 import { LayoutsRoutingModule } from './layouts-routing.module';
 import { PartialsModule } from '../main/partials/partials.module';
 import { PropertyListingsComponent } from '../main/property-listings/property-listings.component';
+import { BuildingsComponent } from '../main/buildings/buildings.component';
 // prettier-ignore
 @NgModule({
      declarations: [
           LayoutsComponent,
           HomeComponent,
-          PropertyListingsComponent
+          PropertyListingsComponent,
+          BuildingsComponent
   ],
      imports: [
           CommonModule,
