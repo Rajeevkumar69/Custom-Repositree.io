83848ef52e3f9847d507952bc2d3d1d199c1489c
RAJEEV KUAMR <96237505+Rajeevkumar69@users.noreply.github.com>
2023-11-09 16:50:40 +0530

    Working on Listing page

--- src/app/views/layouts/layouts.module.ts
+++ src/app/views/layouts/layouts.module.ts
@@ -4,11 +4,13 @@ import { LayoutsComponent } from './layouts.component';
 import { HomeComponent } from '../main/home/home.component';
 import { LayoutsRoutingModule } from './layouts-routing.module';
 import { PartialsModule } from '../main/partials/partials.module';
+import { PropertyListingsComponent } from '../main/property-listings/property-listings.component';
 // prettier-ignore
 @NgModule({
      declarations: [
           LayoutsComponent,
-          HomeComponent
+          HomeComponent,
+          PropertyListingsComponent
   ],
      imports: [
           CommonModule,
---
+++ src/app/views/main/property-listings/property-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-listings',
+  templateUrl: './property-listings.component.html',
+  styleUrls: ['./property-listings.component.scss']
+})
+export class PropertyListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
